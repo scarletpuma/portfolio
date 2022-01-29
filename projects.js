7611127f0526d1daf1255cd65ecbd226036ffd31
Scarlet Puma <scarlt.puma@gmail.com>
2022-01-29 12:19:49 -0500

    project page layout and carousel done

--- projects.js
+++ projects.js
@@ -1,34 +1,67 @@
 $(() => {
-  currentFactIndex = 0;
-  numOfFacts = $(`.facts`).children().length - 1;
+  currentTopProjIndex = 0;
+  numOfTopProj = $(`.topimg`).children().length - 1;
   $(`#next`).on(`click`, () => {
-    $(`.facts`)
+    $(`.topimg`)
       .children()
-      .eq(currentFactIndex)
+      .eq(currentTopProjIndex)
       .css(`display`, `none`)
-    if(currentFactIndex < numOfFacts) {
-        currentFactIndex++
+    if(currentTopProjIndex < numOfTopProj) {
+        currentTopProjIndex++
       } else {
-        currentFactIndex = 0;
+        currentTopProjIndex = 0;
       }
-    $(`.facts`)
+    $(`.topimg`)
       .children()
-      .eq(currentFactIndex)
+      .eq(currentTopProjIndex)
       .css(`display`, `block`)
   })
   $(`#prev`).on(`click`, () => {
-    $(`.facts`)
+    $(`.topimg`)
       .children()
-      .eq(currentFactIndex)
+      .eq(currentTopProjIndex)
       .css(`display`, `none`)
-    if(currentFactIndex > 0) {
-        currentFactIndex--
+    if(currentTopProjIndex > 0) {
+        currentTopProjIndex--
       } else {
-        currentFactIndex = numOfFacts;
+        currentTopProjIndex = numOfTopProj;
       }
-    $(`.facts`)
+    $(`.topimg`)
       .children()
-      .eq(currentFactIndex)
+      .eq(currentTopProjIndex)
+      .css(`display`, `block`)
+  })
+
+  currentBotProjIndex = 0;
+  numOfBotProj = $(`.botimg`).children().length - 1;
+  $(`#next`).on(`click`, () => {
+    $(`.botimg`)
+      .children()
+      .eq(currentBotProjIndex)
+      .css(`display`, `none`)
+    if(currentBotProjIndex < numOfBotProj) {
+        currentBotProjIndex++
+      } else {
+        currentBotProjIndex = 0;
+      }
+    $(`.botimg`)
+      .children()
+      .eq(currentBotProjIndex)
+      .css(`display`, `block`)
+  })
+  $(`#prev`).on(`click`, () => {
+    $(`.botimg`)
+      .children()
+      .eq(currentBotProjIndex)
+      .css(`display`, `none`)
+    if(currentBotProjIndex > 0) {
+        currentBotProjIndex--
+      } else {
+        currentBotProjIndex = numOfBotProj;
+      }
+    $(`.botimg`)
+      .children()
+      .eq(currentBotProjIndex)
       .css(`display`, `block`)
   })
 });
